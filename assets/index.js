c9cdfa6f1e26b328cd56868ad7a45f261228340b
RodVixCosta <rodrigocosta4409@gmail.com>
2022-01-22 18:19:45 -0300

    Co-authored-by: henriquehiga <henriquehiga@users.noreply.github.com>

---
+++ assets/index.js
@@ -0,0 +1,14 @@
+
+AOS.init();
+type="text/javascript">
+$(document).ready(function() {
+$("#instagram").click(function() {
+        window.location = "https://www.instagram.com/irmao_da_lorenna/";
+    });
+});
+type="text/javascript">
+$(document).ready(function() {
+$("#linkedin").click(function() {
+        window.location = "https://www.linkedin.com/in/rodrigo-da-vitoria-23b694227";
+    });
+});
